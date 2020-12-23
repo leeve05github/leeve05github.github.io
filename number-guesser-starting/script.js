0bb06c56c4e59a9429572b24f8ea795a5fbb5ef2
leeve05github <66569986+leeve05github@users.noreply.github.com>
2020-12-24 06:09:24 +0900

    Add files via upload

---
+++ number-guesser-starting/script.js
@@ -0,0 +1,36 @@
+
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+
+
+// generate target number function
+const generateTarget = () => Math.round(Math.random() * 9);
+
+// compare two guesses
+const compareGuesses = (userGuess, computerGuess, targetNum) => {
+
+  if ( Math.abs(userGuess - targetNum) < Math.abs(computerGuess - targetNum) || Math.abs(userGuess - targetNum) === Math.abs(computerGuess - targetNum) ) {
+    return true;
+  } else if (Math.abs(userGuess - targetNum) > Math.abs(computerGuess - targetNum) && Math.abs(computerGuess - targetNum) || Math.abs(userGuess - targetNum) !== Math.abs(computerGuess - targetNum) ) {
+    return false;
+  };
+};
+
+// update score
+const updateScore = (str) => {
+
+  if ('human') {
+    humanScore++;
+  } else if ('computer') {
+  }
+    computerScore++;
+  };
+
+  // advance round 
+
+  const advanceRound = () => currentRoundNumber++;
